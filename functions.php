46e378bc27a75259c3236b275356055ae2208c73
Rajib Mondal <rajib.mondal@technokrats.in>
2018-06-08 23:01:21 +0530

    create theme function

--- functions.php
+++ functions.php
@@ -9,3 +9,5 @@ define('THEME_URI', get_template_directory_uri().'/');
 define('THEME_INC_DIR', THEME_DIR_PATH.'includes/');
 define('JS', THEME_URI.'js/');
 define('CSS', THEME_URI.'css/');
+
+require_once THEME_INC_DIR.'theme-function.php';
