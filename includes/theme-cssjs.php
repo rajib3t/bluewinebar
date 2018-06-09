7c25ac6f12e0f2d43771df45855c9939405b20c3
Rajib Mondal <rajib.mondal@technokrats.in>
2018-06-09 06:11:15 +0530

    add css

--- includes/theme-cssjs.php
+++ includes/theme-cssjs.php
@@ -15,10 +15,14 @@ class Theme_asset {
         wp_enqueue_style('Bootstrap_min', CSS . 'bootstrap.min.css', false, '');
         wp_enqueue_style('font-awesome-min', CSS . 'font-awesome.min.css', false, '');
         wp_enqueue_style('animate-min', CSS . 'animate.min.css', false, '');
-        wp_enqueue_style('Bootstrap_min', CSS . 'bootstrap.min.css', false, '');
+        wp_enqueue_style('slimmenu-min', CSS . 'slimmenu.min.css', false, '');
+        wp_enqueue_style('slick', CSS . 'slick/slick.css', false, '');
+        wp_enqueue_style('slick-theme', CSS . 'slick/slick-theme.css', false, '');
+        wp_enqueue_style('style', CSS . 'style.css', false, '');
+        wp_enqueue_style('responsive', CSS . 'responsive.css', false, '');
+        wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css?family=Roboto:400,500,700', FALSE);
     }
 
 }
-
 
 $theme_asset = new Theme_asset();
