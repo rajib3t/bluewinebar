8318247488dc21ca460a53b0bcc75ca892aba76d
Rajib Mondal <rajib.mondal@technokrats.in>
2018-06-09 06:34:54 +0530

    header modify

--- header.php
+++ header.php
@@ -7,17 +7,21 @@
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-<div id="wrapper" class="hfeed">
-<header id="header" role="banner">
-<section id="branding">
-<div id="site-title"><?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '<h1>'; } ?><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_html( get_bloginfo( 'name' ) ); ?>" rel="home"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></a><?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '</h1>'; } ?></div>
-<div id="site-description"><?php bloginfo( 'description' ); ?></div>
-</section>
-<nav id="menu" role="navigation">
-<div id="search">
-<?php get_search_form(); ?>
-</div>
-<?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
-</nav>
-</header>
-<div id="container">
+<!-- preloader -->
+		<div class="loading">
+			<div class="loading-center">
+				<div class="loading-center-absolute">
+					<div class="preloader-wave">
+						<div class="sk-rect sk-rect1"></div>
+						<div class="sk-rect sk-rect2"></div>
+						<div class="sk-rect sk-rect3"></div>
+						<div class="sk-rect sk-rect4"></div>
+						<div class="sk-rect sk-rect5"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+<div class="main-section">
+    <?php
+    get_template_part('templates/header','menu');
+    ?>
---
+++ templates/header-menu.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * Header Menu and logo template
+ */
+?>
+<!-- Start Header -->
+			<header class="header-section">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-12">
+							<div class="navbar-brand float-left">
+								<a href="http:"><img src="images/logo.png" alt="" /></a>
+							</div>
+							<nav class="nav float-right">
+								<ul id="themeix-nav" class="slimmenu">
+									<li class="active"><a href="index.html">Home</a></li>
+									<li><a href="about.html">About</a></li>
+									<li><a href="contact.html">Contact</a></li>
+									<li><a href="blog-detail.html">Blog Details</a></li>
+									<li><a href="tags.html">Tags</a></li>
+								</ul>
+
+							</nav>
+						</div>
+					</div>
+				</div>
+			</header>
+			<!-- End Header -->
